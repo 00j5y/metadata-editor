47c51787463e7e1cc3caa52a7dcaf7478532d272
jay <jay.lheronde@gmail.com>
2025-03-22 16:28:59 +0100

    Fin MP3 + Debut MP4

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from colorama import Fore, init
 import keyboard
 import time
 import os
-import eyed3
+from mutagen.easyid3 import EasyID3
+from mutagen.mp4 import MP4
 
 # Initalisation de colorama
 init()
@@ -20,33 +21,47 @@ split_tup = os.path.splitext(file_name)
 file_extension = split_tup[1]
 
 def edit_mp3():
-    mp3_file= eyed3.load(file_name)
+    mp3_file = EasyID3(file_name)
     if not mp3_file:
         print(f"{Fore.WHITE}[{Fore.RED}!{Fore.WHITE}] Error loading MP3 file metadata.")
         return
 
     while True:
-        choix = int(input(f"\n{Fore.WHITE}[{Fore.GREEN}!{Fore.WHITE}] Que voulez vous modifié ?\n  1- Artist Name\n  2- Album Name\n  3- Title of the song\n  0- Exit\n  Choice : "))
+        choix = int(input(f"\n{Fore.WHITE}[{Fore.GREEN}!{Fore.WHITE}] What do you want edit ?\n  1- Artist Name\n  2- Album Name\n  3- Title of the song\n  4- Genre of the song\n  5- Release Year of the song\n  0- Exit\n  Choice : "))
         match choix:
             case 1:
                 artist_name = input(f"\n{Fore.WHITE}[{Fore.GREEN}?{Fore.WHITE}] Artist name ? :")
-                mp3_file.tag.artist = artist_name
+                mp3_file["artist"] = artist_name
             case 2: 
                 album_name = input(f"\n{Fore.WHITE}[{Fore.GREEN}?{Fore.WHITE}] Album name ? :")
-                mp3_file.tag.album = album_name
+                mp3_file["album"] = album_name
             case 3:
                 title_song = input(f"\n{Fore.WHITE}[{Fore.GREEN}?{Fore.WHITE}] Title of the song ? :")
-                mp3_file.tag.title = title_song
+                mp3_file["title"] = title_song
+            case 4:
+                genre_song = input(f"\n{Fore.WHITE}[{Fore.GREEN}?{Fore.WHITE}] Genre of the song ? :")
+                mp3_file["genre"] = genre_song
+            case 5:
+                year_song = input(f"\n{Fore.WHITE}[{Fore.GREEN}?{Fore.WHITE}] Release Date ? :")
+                mp3_file["date"] = year_song
             case 0:
                 break
             case _:
                 print(f"\n{Fore.WHITE}[{Fore.RED}!{Fore.WHITE}] Unknow choice ! Try again")
-    mp3_file.tag.save()
+    mp3_file.save()
     print(f"{Fore.WHITE}[{Fore.GREEN}?{Fore.WHITE}] Saved with sucess")
+
+def edit_mp4():
+    mp4_file = MP4(file_name)
+    if not mp4_file:
+        print(f"{Fore.WHITE}[{Fore.RED}!{Fore.WHITE}] Error loading MP4 file metadata.")
+        return
 
 match file_extension:
     case ".mp3":
         edit_mp3()
+    case ".mp4":
+        edit_mp4()
     case _:
         print(f"{Fore.WHITE}[{Fore.GREEN}!{Fore.WHITE}] File Format not supported")
             
